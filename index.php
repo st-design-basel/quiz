585ae7eabc76ed0fa03964d987a6bcdd903e8825
st-design-basel <ST-Design@MacBook-Pro-2.local>
2016-09-21 03:45:37 +0200

    json

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
     
 
-    echo "Banana 2345 new build erer ";
+    
 	
-	$errors = array(
- '{"Question":"What is the capital of France?","Right":"Paris","Wrong1":"bern","Wrong2":"Madrid","Wrong3":"Tokyo"}',
-  '{"ky2": "bar2", "ky1": "foo2"}',
+	$Questios = array(
+ 	'{"Question":"What is the capital of France?","Right":"Paris","Wrong1":"bern","Wrong2":"Madrid","Wrong3":"Tokyo"}',
+  	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
   
 );
 
-foreach ($errors as $error)
-{
-    echo "<br />Error: " . $error;
-}
-echo "Banana 2345 new build erer ";
 
-echo json_encode($errors);
+
+echo json_encode($Questios);
 ?>
