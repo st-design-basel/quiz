2e3be913d4d93d33c1900f9b81f99c2dfd3c2879
st-design-basel <ST-Design@MacBook-Pro-2.local>
2016-09-21 03:29:10 +0200

    no error

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     echo "Banana 2345 new build erer ";
 	
 	$errors = array(
-    array('{"ky2": "bar", "ky1": "foo"}'),
-    array('{"ky2": "bar2", "ky1": "foo2"}'),
+    '{"ky2": "bar", "ky1": "foo"}',
+    '{"ky2": "bar2", "ky1": "foo2"}',
 );
 
 foreach ($errors as $error)
