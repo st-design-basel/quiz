1d0dfd9cb596d1836000bf84fb9526903741e4a9
st-design-basel <ST-Design@MacBook-Pro-2.local>
2016-09-21 14:46:11 +0200

    sumi

--- index.php
+++ index.php
@@ -4,27 +4,53 @@
     
 	
 	$Questios = array(
- 	'{"Question":"What is the capital of France?","Right":"Paris","Wrong1":"bern","Wrong2":"Madrid","Wrong3":"Tokyo"}',
-  	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
-	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+
+	
+
+	
+	'{"Question":"What is the capital of France?","Right":"Paris","Wrong1":"bern","Wrong2":"Madrid","Wrong3":"Tokyo"}',
+        
+        '{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+
+        '{"Question":"Which state is the biggest in the US?","Right":"Alaska","Wrong1":"Connecticut","Wrong2":"California","Wrong3":"Missouri"}',
+
+        '{"Question":"Which is the country where reggae music originated?","Right":"Jamaica","Wrong1":"Brazil","Wrong2":"Spain","Wrong3":"Madagascar"}',
+
+        '{"Question":"How many legs do butterflies have?","Right":"6","Wrong1":"4","Wrong2":"8","Wrong3":"12"}',
+
+        '{"Question":"Which is the country hosting the 2008 Olympic Games?","Right":"China","Wrong1":"England","Wrong2":"Germany","Wrong3":"Switzerland"}',
+
+        '{"Question":"What colour do you add to blue to make it green?","Right":"Yellow","Wrong1":"Red","Wrong2":"White","Wrong3":"Orange"}',
+
+        '{"Question":"What is the name of Mickey Mouses pet dog?","Right":"Pluto","Wrong1":"idefix","Wrong2":"milou","Wrong3":"Lassie"}',
+
+        '{"Question":"Which fabric is made by worms?","Right":"Silk","Wrong1":"Wool","Wrong2":"Leather","Wrong3":"Cotton"}',
+  
+        '{"Question":"How many milligrams are in one gram?","Right":"1000","Wrong1":"100","Wrong2":"1","Wrong3":"10"}',
+  
+        '{"Question":"Which Apollo 11 astronaut did not set foot on the moon?","Right":"Michael Collins","Wrong1":"Neil Armstrong","Wrong2":"Buzz Aldrin","Wrong3":"10"}',
+
+        '{"Question":"In which French city did the Popes reside during the 14th century?","Right":"Avignon","Wrong1":"Paris","Wrong2":"Lyon","Wrong3":"10"}',
+
+        '{"Question":"Which hot and spicy brand name does one associate with Avery island?","Right":"Tabasco","Wrong1":"Thommy","Wrong2":"Redbull","Wrong3":"10"}',
+ 
+        '{"Question":"With which profession does one associate the Stanislawski method?","Right":"Acting","Wrong1":"Singing","Wrong2":"Finance","Wrong3":"10"}',
+
+        '{"Question":"Who was the last man to win the grand slam in tennis?","Right":"Rod Laver","Wrong1":"Nadal","Wrong2":"roger federer","Wrong3":"10"}',
+
+        '{"Question":"What would you be eating if you ordered LENGUA in Spain ?","Right":"Tongue","Wrong1":"Tapas","Wrong2":"Fries","Wrong3":"10"}',
+
+        '{"Question":"The Germanic goddess of the springtime Ostara was what kind animal?","Right":"A rabbit","Wrong1":"a Cat","Wrong2":"A mouse","Wrong3":"10"}',
+
+        '{"Question":"Which European countrys name stems from the Germanic word for foreig?","Right":"Wales","Wrong1":"Germany","Wrong2":"Spain","Wrong3":"10"}',
+
+        '{"Question":"Which is the largest country in Afreca?","Right":"Algeria","Wrong1":"Sudan","Wrong2":"Mali","Wrong3":"10"}',
+
+        '{"Question":"Port Said is in which North African country?","Right":"Egypt","Wrong1":"Tunisia","Wrong2":"Marroco","Wrong3":"10"}',
+
+        '{"Question":"Granadilla is another name for which fruit?","Right":"Passion-fruit","Wrong1":"Mongo","Wrong2":"Bannanas","Wrong3":"10"}'
+        
+
   
 );
 
---
+++ index2.php
@@ -0,0 +1,34 @@
+<?php
+    
+
+    
+	
+	$Questios = array(
+ 	'{"Question":"What is the capital of France?","Right":"Paris","Wrong1":"bern","Wrong2":"Madrid","Wrong3":"Tokyo"}',
+  	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+	'{"Question":"Which indoor sport is the most popular in the US?","Right":"Basketball","Wrong1":"Golf","Wrong2":"volleyball","Wrong3":"ice hockey"}',
+  
+);
+
+
+
+echo json_encode($Questios);
+?>
